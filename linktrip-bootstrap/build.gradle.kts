e5726220e66e9429ee852f4a0e6c5bcb2a444cfd
toychip <dlawnsgud427@naver.com>
2026-02-25 18:22:32 +0900

    setting: bootBuildImage ECR 배포 설정

--- linktrip-bootstrap/build.gradle.kts
+++ linktrip-bootstrap/build.gradle.kts
@@ -18,7 +18,44 @@ tasks {
         enabled = true
     }
 
+    named("bootJar") {
+        dependsOn("ktlintFormat")
+    }
+
     getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
         mainClass.set("com.linktrip.bootstrap.LinktripBootstrapApplicationKt")
     }
+
+    getByName<org.springframework.boot.gradle.tasks.bundling.BootBuildImage>("bootBuildImage") {
+        imageName.set("${System.getenv("ECR_REGISTRY")}/${System.getenv("ECR_REPOSITORY")}")
+        environment.set(
+            mapOf(
+                "BP_JVM_VERSION" to "21",
+                "BPE_SPRING_PROFILES_ACTIVE" to "prod",
+                "BPE_JAVA_TOOL_OPTIONS" to
+                    buildString {
+                        append("-XX:+UseG1GC ")
+                        append("-XX:+UseContainerSupport ")
+                        append("-Xms512m -Xmx512m ")
+                        append("-XX:+HeapDumpOnOutOfMemoryError ")
+                        append("-XX:HeapDumpPath=/root/heapDump/%Y%m%d_%H%M%S.hprof ")
+                        append("-XX:+UseStringDeduplication ")
+                        append("-XX:+ExitOnOutOfMemoryError ")
+                        append("-Dfile.encoding=UTF-8")
+                    },
+            ),
+        )
+        docker {
+            publishRegistry {
+                url.set(System.getenv("ECR_REGISTRY"))
+                username.set("AWS")
+                password.set(System.getenv("ECR_PASSWORD") ?: "")
+            }
+        }
+        publish.set(true)
+    }
+}
+
+springBoot {
+    buildInfo()
 }
