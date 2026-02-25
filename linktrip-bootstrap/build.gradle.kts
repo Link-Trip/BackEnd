47d6d8540db024698cb6009211bab0f7d79fd2aa
toychip <dlawnsgud427@naver.com>
2026-02-25 17:56:19 +0900

    setting: redis 패키지, 의존성 임시 주석 처리

--- linktrip-bootstrap/build.gradle.kts
+++ linktrip-bootstrap/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 dependencies {
     implementation(project(":linktrip-input-http"))
     implementation(project(":linktrip-output-persistence:mysql"))
-    implementation(project(":linktrip-output-cache:redis"))
+//    implementation(project(":linktrip-output-cache:redis"))
     implementation(project(":linktrip-output-storage:aws"))
 
     implementation(libs.bundles.bootstrap)
