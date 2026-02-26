c2a408af4bd5e112768cb833d7c7fe11eb2d6ba3
toychip <dlawnsgud427@naver.com>
2026-02-26 15:46:45 +0900

    feat: Swagger Api Docs (#7)

---
+++ linktrip-input-http/src/main/kotlin/com/linktrip/input/http/config/OpenApiConfiguration.kt
@@ -0,0 +1,41 @@
+package com.linktrip.input.http.config
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenApiConfiguration {
+    @Bean
+    fun openApi(): OpenAPI {
+        val info =
+            Info()
+                .title(API_NAME)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION)
+
+        val prod =
+            Server()
+                .url("https://linktrip.cloud")
+                .description("Production")
+
+        val local =
+            Server()
+                .url("http://localhost:8080")
+                .description("Local")
+
+        return OpenAPI()
+            .components(Components())
+            .info(info)
+            .servers(listOf(prod, local))
+    }
+
+    companion object {
+        const val API_NAME = "LinkTrip API"
+        const val API_VERSION = "v1"
+        const val API_DESCRIPTION = "LinkTrip API"
+    }
+}
