f34272ae8b3f616afc847aa77f15bc066b02470c
toychip <dlawnsgud427@naver.com>
2026-03-02 16:05:38 +0900

    feat: 테스트용 예외 발생 API 추가 (#8)

--- linktrip-common/src/main/kotlin/com/linktrip/common/exception/ExceptionCode.kt
+++ linktrip-common/src/main/kotlin/com/linktrip/common/exception/ExceptionCode.kt
@@ -25,4 +25,5 @@ enum class ExceptionCode(
 
     // 500
     INTERNAL_SERVER_ERROR(500, "서버 내부 오류가 발생했습니다."),
+    ERROR_TEST(500, "에러 테스트용 예외입니다."),
 }
--- linktrip-input-http/src/main/kotlin/com/linktrip/input/http/controller/health/HealthCheckController.kt
+++ linktrip-input-http/src/main/kotlin/com/linktrip/input/http/controller/health/HealthCheckController.kt
@@ -1,10 +1,13 @@
 package com.linktrip.input.http.controller.health
 
 import com.linktrip.application.port.input.HealthCheckUseCase
+import com.linktrip.common.exception.ExceptionCode
+import com.linktrip.common.exception.LinktripException
 import com.linktrip.input.http.controller.dto.response.ApiResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -33,6 +36,19 @@ class HealthCheckController(
                     data = HealthResponse(status = healthStatus, service = "linktrip-db"),
                 ),
             )
+    }
+
+    @PostMapping("/error-test-global")
+    fun errorTestGlobal() {
+        throw RuntimeException()
+    }
+
+    @PostMapping("/error-test-linktrip")
+    fun errorTestLinkTrip() {
+        throw LinktripException(
+            ExceptionCode.ERROR_TEST,
+            "error-test: 의도적으로 발생시킨 LinktripException",
+        )
     }
 
     data class HealthResponse(
