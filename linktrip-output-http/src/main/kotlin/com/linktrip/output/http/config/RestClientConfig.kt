0941fed8085ec95080e93629d1629688d53e8585
toychip <dlawnsgud427@naver.com>
2026-03-11 13:56:19 +0900

    feat: RestClient Named Bean 리팩토링 및 HttpClient 제거 (#19)

--- linktrip-output-http/src/main/kotlin/com/linktrip/output/http/config/RestClientConfig.kt
+++ linktrip-output-http/src/main/kotlin/com/linktrip/output/http/config/RestClientConfig.kt
@@ -6,6 +6,21 @@ import org.springframework.web.client.RestClient
 
 @Configuration
 class RestClientConfig {
-    @Bean
-    fun restClient(): RestClient = RestClient.builder().build()
+    @Bean("googlePlacesRestClient")
+    fun googlePlacesRestClient(): RestClient =
+        RestClient.builder()
+            .baseUrl(GOOGLE_PLACES_BASE_URL)
+            .defaultHeader("X-Goog-FieldMask", GOOGLE_PLACES_FIELD_MASK)
+            .build()
+
+    @Bean("discordRestClient")
+    fun discordRestClient(): RestClient =
+        RestClient.builder()
+            .build()
+
+    companion object {
+        private const val GOOGLE_PLACES_BASE_URL = "https://places.googleapis.com/v1"
+        private const val GOOGLE_PLACES_FIELD_MASK =
+            "places.id,places.displayName,places.formattedAddress,places.location"
+    }
 }
