5fe1182d9a5f247ce510ca8a1ac1c7045cf39eab
toychip <dlawnsgud427@naver.com>
2026-03-25 17:47:49 +0900

    feat: Member 영속성 계층 OAuth 필드 반영 (#42)

--- linktrip-application/src/main/kotlin/com/linktrip/application/port/output/persistence/MemberPort.kt
+++ linktrip-application/src/main/kotlin/com/linktrip/application/port/output/persistence/MemberPort.kt
@@ -1,9 +1,15 @@
 package com.linktrip.application.port.output.persistence
 
 import com.linktrip.application.domain.member.Member
+import com.linktrip.application.domain.member.ProviderType
 
 interface MemberPort {
-    fun findBySerialNumber(serialNumber: String): Member?
+    fun findByProviderTypeAndProviderId(
+        providerType: ProviderType,
+        providerId: String,
+    ): Member?
+
+    fun findByEmail(email: String): Member?
 
     fun save(member: Member): Member
 }
--- linktrip-output-persistence/mysql/src/main/kotlin/com/linktrip/output/persistence/mysql/adapter/MemberAdapter.kt
+++ linktrip-output-persistence/mysql/src/main/kotlin/com/linktrip/output/persistence/mysql/adapter/MemberAdapter.kt
@@ -1,6 +1,7 @@
 package com.linktrip.output.persistence.mysql.adapter
 
 import com.linktrip.application.domain.member.Member
+import com.linktrip.application.domain.member.ProviderType
 import com.linktrip.application.port.output.persistence.MemberPort
 import com.linktrip.output.persistence.mysql.entity.MemberEntity
 import com.linktrip.output.persistence.mysql.repository.MemberJpaRepository
@@ -10,8 +11,12 @@ import org.springframework.stereotype.Component
 class MemberAdapter(
     private val memberJpaRepository: MemberJpaRepository,
 ) : MemberPort {
-    override fun findBySerialNumber(serialNumber: String): Member? =
-        memberJpaRepository.findBySerialNumber(serialNumber)?.toDomain()
+    override fun findByProviderTypeAndProviderId(
+        providerType: ProviderType,
+        providerId: String,
+    ): Member? = memberJpaRepository.findByProviderTypeAndProviderId(providerType, providerId)?.toDomain()
+
+    override fun findByEmail(email: String): Member? = memberJpaRepository.findByEmail(email)?.toDomain()
 
     override fun save(member: Member): Member = memberJpaRepository.save(MemberEntity.from(member)).toDomain()
 }
